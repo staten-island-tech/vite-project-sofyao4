9ca9aa47091f54f7d9fa63e03339527653917544
sofyao4 <113201319+sofyao4@users.noreply.github.com>
2022-12-12 13:13:08 -0500

    12/12

--- vite-project/js/dom.js
+++ vite-project/js/dom.js
@@ -1,9 +1,7 @@
 const DOMSelectors = {
-  divMain: document.getElementsByClassName(".contents"),
   h1: document.getElementsByClassName(".h1"),
   h2: document.getElementsByClassName(".h2"),
-  list: document.getElementsByClassName(".list-products"),
-  listItem: document.getElementsByClassName(".list-item"),
   test: "test1",
+  divMain: document.getElementById("content-box"),
 };
 export { DOMSelectors };
--- vite-project/js/main.js
+++ vite-project/js/main.js
@@ -2,4 +2,13 @@ import { DOMSelectors } from "./dom.js";
 import { products } from "./products.js";
 import "../styles/style.css";
 //dont forget about encapsulation of functions in an object.
-products.forEach((object) => console.log(object.name));
+
+function productCard() {
+  products.forEach((product) => {
+    DOMSelectors.divMain.insertAdjacentHTML(
+      "beforeend",
+      `<p> ${product.name} </p>`
+    );
+  });
+}
+productCard();
